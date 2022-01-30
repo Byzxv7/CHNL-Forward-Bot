71c5905ff0dc8dd0fd2a0e9dc5ae51e09eb41976
Byzxv7 <92144730+Byzxv7@users.noreply.github.com>
2022-01-30 18:44:39 +0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-100"))
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5034247920:AAFxkf4LPKnurMzkvUH5st8QysvQrZTwaZE")
+    APP_ID = int(os.environ.get("APP_ID", 10905445))
+    API_HASH = os.environ.get("API_HASH", "6a8cead041f2b7640dbb9644dc41ecb0")
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001543466659"))
